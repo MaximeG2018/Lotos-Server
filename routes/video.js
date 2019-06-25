ffc08f2c2992bfc6ab32d24f37f34a89d291a753
MaximeG2018 <maxime.gouenard@gmail.com>
2019-06-25 15:44:08 +0200

    add route categorie

--- routes/video.js
+++ routes/video.js
@@ -9,9 +9,26 @@ api.get('/all', (req, res) => {
   const url = 'https://r3lovution-lotus-api.herokuapp.com/api/videos';
   fetch(url,res)
 })
+
 api.get('/:categorie', (req, res) => {
   console.log(req.params.catergorie)
   const url = 'https://r3lovution-lotus-api.herokuapp.com/api/videos';
   fetch(url,res)
 })
+
+function fetch(url, res) {
+	  axios.get(url, {
+	    headers: {
+	        Accept: "application/json"
+	    }
+	  })
+	  .then(response => {
+	    res.send(response.data);
+	  })
+	  .catch(err => {
+	    let json = circularJson.stringify(err.response);
+	    res.send(JSON.parse(json))
+	  });
+	}
+
 export default api;
