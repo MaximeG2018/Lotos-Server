d3ddb053b7fe2c2460a67b3294e171cf697a96ab
ibrahima DANSOKO <ibrahima.dansoko@ftprod.fr>
2019-06-27 13:03:30 +0200

    manual merge yass bdd and sequelizePostgres

--- routes/video.js
+++ routes/video.js
@@ -1,34 +1,35 @@
-import Video from '../database/model/video'
-const express = require('express');
-const axios = require('axios');
-const circularJson = require('circular-json');
+import Video from "../database/model/video";
+const express = require("express");
+const axios = require("axios");
+const circularJson = require("circular-json");
 
 const api = express();
 
-api.get('/all', (req, res) => {
-  const url = 'https://r3lovution-lotus-api.herokuapp.com/api/videos';
-  fetch(url,res)
-})
+api.get("/all", (req, res) => {
+  const url = "https://r3lovution-lotus-api.herokuapp.com/api/videos";
+  fetch(url, res);
+});
 
-api.get('/:categorie', (req, res) => {
-  console.log(req.params.categorie)
-  const url = 'https://r3lovution-lotus-api.herokuapp.com/api/videos';
-  fetch(url,res)
-})
+api.get("/:categorie", (req, res) => {
+  console.log(req.params.categorie);
+  const url = "https://r3lovution-lotus-api.herokuapp.com/api/videos";
+  fetch(url, res);
+});
 
 function fetch(url, res) {
-	  axios.get(url, {
-	    headers: {
-	        Accept: "application/json"
-	    }
-	  })
-	  .then(response => {
-	    res.send(response.data);
-	  })
-	  .catch(err => {
-	    let json = circularJson.stringify(err.response);
-	    res.send(JSON.parse(json))
-	  });
-	}
+  axios
+    .get(url, {
+      headers: {
+        Accept: "application/json"
+      }
+    })
+    .then(response => {
+      res.send(response.data);
+    })
+    .catch(err => {
+      let json = circularJson.stringify(err.response);
+      res.send(JSON.parse(json));
+    });
+}
 
 export default api;
